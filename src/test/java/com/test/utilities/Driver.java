29994dedf4b78425ba44358af2ffe06470048b77
KyleEsBa <kyleb.-95@hotmail.com>
2024-09-30 17:26:35 -0500

    Updated web driver manager + added simple singleton driver without parallel testing implementation as a comment only

--- src/test/java/com/test/utilities/Driver.java
+++ src/test/java/com/test/utilities/Driver.java
@@ -39,3 +39,55 @@ public class Driver {
         }
     }
 }
+
+/*
+//***************      SIMPLE DRIVER         *******************
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+   private static WebDriver driver;
+
+     public static WebDriver getDriver() {
+         //  public static WebDriver getDriver(String browserType) {
+
+         String browserType = ConfigurationReader.getProperties("browser");
+
+        if (driver == null) {
+
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "FireFox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+
+        if (driver!=null){
+            driver.quit();
+
+            driver=null;
+        }
+
+    }
+
+}*/
